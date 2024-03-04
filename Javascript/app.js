02045b49499db5d18c5a0431f748a75eef439d32
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-03-04 16:40:04 +0500

    Practice with JS

---
+++ Javascript/app.js
@@ -0,0 +1,137 @@
+// ========================Chapter#1==============================
+// Q1
+alert("Greetings From Developer");
+
+// Q2
+alert("JavaScript Alert\n Error! Enter valid Password");
+
+// Q3
+alert("Welcome to JS Land\n Happy Coding!");
+
+// Q4(i)
+alert("Welcome to JS Land")
+
+// Q4(ii)
+alert("prevent this page from creating additional dialogs.");
+
+// Q5
+
+alert("JavaScript Alert\n Hello... I can run JS through my web brwoser's console");
+
+
+// ====================================Chapter 2=============================================
+// 1. Declare a variable called username.
+var username= "Abdul Wadood";
+
+//2. Declare a variable called myName & assign to it a string that represents your Full Name.
+var myName = username;
+
+// 3.3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+var message = jsVar;
+var jsVar= "Hello World";
+alert(jsVar);
+
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+var stu_Name= "Abdul-Wadood";
+var stu_Age = "22 years old";
+var certified ="Certified Mobile Application Development";
+
+alert(stu_Name);
+alert(stu_Age);
+alert(certified);
+
+// 5. Write a script to display the following alert using one JS
+// variable(Pizza):
+var pizza=("PIZZA\n PIZZ\n PIZ\n PI\n P");
+alert(pizza);
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+var email = "abdulwadood0317@gmail.com";
+alert("My email address is "+email);
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+var book= "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book"+book);
+
+// 8. Write a script to display this in browser through JS
+document.write("I can write HTML content through Javascript");
+
+// 9. Store following string in a variable and show in alert and browser through JS
+var dummy_str= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(dummy_str);
+document.write(dummy_str)
+
+
+
+
+
+
+// ========================Chapter 03===========================
+
+//Question No. 01
+// number variable already declare
+alert("I am " + student_Age + " year old");
+//Question No. 02
+var visitors = 104;
+alert("You have visisted this site " + visitors + " times");
+//Question No. 03
+var dob = 2002;
+document.write("<br> My birth year is " + dob);
+document.write("<br> Date type of my declare variable is number")
+//Question No. 04
+var visitor_Name = "Abdul Wadood";
+var product_Title = "T-Shirt";
+var purchase_quantity = 5;
+document.write("<br>" + visitor_Name + " ordered " + purchase_quantity + product_Title + " (s) on XYZ Clothing store");
+
+// ========================Chapter 04===========================
+
+//Question No. 01
+var first_Name = "Abdul", last_Name = "Wadood", full_Name = first_Name + " " + last_Name;
+//Question No. 02
+
+// Legal variable name
+
+var customer_Number = 10025;
+var customer_Name = "Ali";
+var customer_Dob = 2000;
+var customer_City = "Karachi";
+var customer_Status = "Active";
+
+// Illegal variable name
+
+// Temporary Comment
+
+// var Customer_# = 10023;
+// var Customer_Name = "Farooq";
+// var DateofBirth = 1988;
+// var @_city = "Karachi";
+// var Customer-Status = "Block";
+
+//Question No. 03
+
+var rules = "Rules for naming JS variables";
+document.write("<h1> " + rules + " </h1>");
+var numbers = "numbers";
+var $sign = "$";
+var _sign = "_";
+document.write("<br> Variable names can only contain" + numbers+ "," + $sign + "," + " and " + _sign + "." + " For example : " + $sign + "my" + _sign + "$my_1stVariable" );
+document.write("<br> Variables must begin with a " + $sign+ " or " + _sign + " For example : " + $sign + "name" + "," + _sign + "name or name" );
+var variable_Name = "sensitive";
+document.write("<br> Variable names are case " + variable_Name);
+var js_Keywords = "keywords";
+document.write("<br> Variable names should not be JS " + js_Keywords);
+
+
+
