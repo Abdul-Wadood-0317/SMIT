2f5c8f9e71d5816880bf03f7f6b88a38fe0c2578
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-03-15 16:34:37 +0500

    Js Basics

--- JS_HandsOn/01_basics/01_var.js
+++ JS_HandsOn/01_basics/01_var.js
@@ -1,1 +1,15 @@
-console.log("Love you Sir");
+let studId = 1;
+let studName = "Abdul Wadood";
+var studEmail = "abc@gmail.com";
+const studPassword = "1234";
+let studCity = 'Lahore';
+
+studName = "Ahmed";
+studEmail = "12@gmail.com";
+studCity = "Karachi";
+let studGrade;
+
+console.log([studId]);
+
+console.table([studName,studEmail,studCity,studPassword,studGrade])
+console.log(typeof studEmail);
