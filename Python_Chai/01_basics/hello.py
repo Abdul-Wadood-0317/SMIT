40784f2a44ae314ddf995069a1fd688fb7639888
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-03-17 23:59:04 +0500

    Py Basics

--- Python_Chai/01_basics/hello.py
+++ Python_Chai/01_basics/hello.py
@@ -1,7 +1,11 @@
-print("Hello Wadood again")
+# print("Hello Wadood again")
 
 
 def tea(n):
     print(n)
 
 tea("Dates and almond")
+
+tea_one = "Doodh pati"
+tea_two = "lemon tea"
+tea_three = "ginger tea"
