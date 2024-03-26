a81ded807af46b958fd140d2ab11305ce51647c0
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-03-26 11:18:22 +0500

    Str Basics

--- JS_Chai/01_basics/06_nums_and_maths.js
+++ JS_Chai/01_basics/06_nums_and_maths.js
@@ -1,13 +1,37 @@
 const score = 400;
-console.log(score);
+// console.log(score);
 
 const balance =  new Number(100)
-console.log(balance);
+// console.log(balance);
 
 console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2));
 
 const otherNum = 123.48927;
-console.log(otherNum.toPrecision(4));
+// console.log(otherNum.toPrecision(4));
+
+const hundreds = 100000000;
+// console.log(hundreds.toLocaleString('en-PK'));
+
+
+
+//+++++++++++++++  Maths   +++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-34));
+// console.log(Math.round(3.9));
+// console.log(Math.ceil(2.1));
+// console.log(Math.floor(4.5));
+// console.log(Math.sqrt(2));
+// console.log(Math.min(2,3,5,1,6));
+// console.log(Math.max(2,3,5,1,6));
+
+console.log(Math.random());
+console.log((Math.random()*10)  +1);
+console.log(Math.floor(Math.random()*10)  +1);
+
+const max = 10
+const min = 30
+console.log(Math.floor(Math.random() *(max-min+1))+min);
 
 
