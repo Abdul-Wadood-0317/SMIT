655c5672256f28fd8e930ce8dfd150a4b10edee7
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-03-27 11:26:16 +0500

    Str Basics

---
+++ JS_Chai/01_basics/07_dates_inJS.js
@@ -0,0 +1,18 @@
+//Dates
+
+let myDate = new Date()
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.getMilliseconds());
+// console.log(myDate.getDate());
+// console.log(typeof(myDate));
+
+let myCreateDate = new Date (2024,2,27)
+console.log(myCreateDate.toDateString());
