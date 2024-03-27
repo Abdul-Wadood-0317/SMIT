6342577ee5db2c24cfd33df15188e49848a7d658
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-03-27 17:21:01 +0500

    push,pop,slice,splice,shift,unshift,indefof,includes,typeof

--- JS_Chai/02_basics/01_arrays.js
+++ JS_Chai/02_basics/01_arrays.js
@@ -3,16 +3,34 @@
 const myArray = [0,1,2,3,4,5]
 console.log(myArray[2]);
 
-const myArr = new Array(1,2,3,4)
-console.log(myArr[0]);
+const myArr = new Array(0,1,2,3,4,5)
+// console.log(myArr[0]);
 
-myArr.push(5)
-myArr.pop(6)
-console.log(myArr);
+// myArr.push(5)
+// myArr.pop(6)
+// console.log(myArr);
 
-myArr.unshift(121)
-myArr.shift()
-console.log(myArr.includes(10));
-console.log(myArr.indexOf(4));
+// myArr.unshift(121)
+// myArr.shift()
+// console.log(myArr.includes(10));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+
+// console.log( typeof myArr);
+// console.log(typeof newArr);
 
 // console.log(myArr);
+
+
+//slice, splice
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C",myArr);
+console.log(myn2);
