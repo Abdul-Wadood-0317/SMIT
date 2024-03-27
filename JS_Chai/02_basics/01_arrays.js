cba8d4d85aff0e1c3772ea2fd2f5906d5032bc0d
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-03-27 16:06:03 +0500

    Array basic

--- JS_Chai/02_basics/01_arrays.js
+++ JS_Chai/02_basics/01_arrays.js
@@ -2,3 +2,12 @@
 
 const myArray = [0,1,2,3,4,5]
 console.log(myArray[2]);
+
+const myArr = new Array(1,2,3,4)
+console.log(myArr[0]);
+
+myArr.push(5)
+myArr.pop(6)
+console.log(myArr);
+
+// console.log(myArr);
