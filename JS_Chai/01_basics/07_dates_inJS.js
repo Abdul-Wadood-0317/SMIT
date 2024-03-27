b8816d2458a86217462474df447e76ab518a279d
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-03-27 15:33:51 +0500

    Intro to arrays

--- JS_Chai/01_basics/07_dates_inJS.js
+++ JS_Chai/01_basics/07_dates_inJS.js
@@ -14,5 +14,19 @@ let myDate = new Date()
 // console.log(myDate.getDate());
 // console.log(typeof(myDate));
 
-let myCreateDate = new Date (2024,2,27)
-console.log(myCreateDate.toDateString());
+let myCreateDate = new Date ("03-27-2024")
+// console.log(myCreateDate.toDateString());
+// console.log(myCreateDate.toLocaleString());
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+// console.log(myCreateDate.getDate());
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getMonth() +1);
+console.log(newDate.getTime());
